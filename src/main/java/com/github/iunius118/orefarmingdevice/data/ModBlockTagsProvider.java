eb65e3a8b9296233790856542a958b82fce2ffce
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-06-29 19:20:12 +0900

    Fix mod block tags provider

--- src/main/java/com/github/iunius118/orefarmingdevice/data/ModBlockTagsProvider.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/data/ModBlockTagsProvider.java
@@ -3,10 +3,15 @@ package com.github.iunius118.orefarmingdevice.data;
 import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
 import com.github.iunius118.orefarmingdevice.world.level.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.TagAppender;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagsProvider extends BlockTagsProvider {
@@ -16,6 +21,15 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.DEVICE_0).add(ModBlocks.DEVICE_1).add(ModBlocks.DEVICE_2).add(ModBlocks.COBBLESTONE_DEVICE_0);
+        addBlocks(tag(BlockTags.MINEABLE_WITH_PICKAXE),
+                ModBlocks.DEVICE_0, ModBlocks.DEVICE_1, ModBlocks.DEVICE_2, ModBlocks.COBBLESTONE_DEVICE_0);
+    }
+
+    private TagAppender<Block> addBlocks(TagAppender<Block> appender, Block... blocks) {
+        Arrays.stream(blocks)
+                .map(b -> BuiltInRegistries.BLOCK.wrapAsHolder(b).getKey())
+                .filter(Objects::nonNull)
+                .forEach(appender::add);
+        return appender;
     }
 }
