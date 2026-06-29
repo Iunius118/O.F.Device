720fd768dc5ddcf4859a229c86562f24c0c3d177
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-06-30 01:04:14 +0900

    Fix mod block tags provider

--- src/main/java/com/github/iunius118/orefarmingdevice/data/ModBlockTagsProvider.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/data/ModBlockTagsProvider.java
@@ -3,20 +3,35 @@ package com.github.iunius118.orefarmingdevice.data;
 import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
 import com.github.iunius118.orefarmingdevice.world.level.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.TagAppender;
+import net.minecraft.data.tags.VanillaBlockTagsProvider;
 import net.minecraft.tags.BlockTags;
-import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-public class ModBlockTagsProvider extends BlockTagsProvider {
+public class ModBlockTagsProvider extends VanillaBlockTagsProvider {
     public ModBlockTagsProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper exFileHelper) {
         super(packOutput, lookupProvider, OreFarmingDevice.MOD_ID, exFileHelper);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.DEVICE_0).add(ModBlocks.DEVICE_1).add(ModBlocks.DEVICE_2).add(ModBlocks.COBBLESTONE_DEVICE_0);
+        addBlocks(tag(BlockTags.MINEABLE_WITH_PICKAXE),
+                ModBlocks.DEVICE_0, ModBlocks.DEVICE_1, ModBlocks.DEVICE_2, ModBlocks.COBBLESTONE_DEVICE_0);
+    }
+
+    private TagAppender<Block> addBlocks(TagAppender<Block> appender, Block... blocks) {
+        Arrays.stream(blocks)
+                .map(b -> BuiltInRegistries.BLOCK.wrapAsHolder(b).unwrapKey())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .forEach(appender::add);
+        return appender;
     }
 }
